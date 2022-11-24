34941d81494eb439b9632fe5ca1677e9726d4a97
Sang <hoangsang2020@gmail.com>
2022-11-24 15:47:47 +0700

    update code

--- app.py
+++ app.py
@@ -86,10 +86,15 @@ if __name__ == '__main__':
     Input variables from command line
     Usage:
         options:
-                -h, --help           show this help message and exit
-                --driver DRIVER      Run driver.py to check
-                --server SERVER      Server's name
-                --database DATABASE  Database's name
+                    -h, --help            show this help message and exit
+                    -d DRIVER, --driver DRIVER
+                                            Run driver.py to check
+                    -s SERVER, --server SERVER
+                                            Server's name
+                    -D DATABASE, --database DATABASE
+                                            Database's name
+                    -m, --mode            True: push csv on database;False: get table on database and save
+                    -p PATH, --path PATH  csv's path
     '''
     parser = arg.ArgumentParser(description="COMUNICATE-SERVER-CLIENT")
     parser.add_argument('-d','--driver',help = "Run driver.py to check",required=True)
